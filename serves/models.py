a4b629dcca11338875ade6798257bec0b9569043
Demianight <komarovdemian@yandex.ru>
2023-11-01 01:40:06 +0300

    feat: date added to models

--- serves/models.py
+++ serves/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from .consts import ANNOUNCEMENT_CHOICES, POST_CHOICES
 
@@ -11,6 +12,7 @@ class Announcement(models.Model):
     link = models.URLField()
     action = models.CharField(max_length=256, choices=ANNOUNCEMENT_CHOICES)
     length = models.SmallIntegerField()
+    date = models.DateTimeField(default=timezone.now)
 
 
 class Post(models.Model):
@@ -22,3 +24,4 @@ class Post(models.Model):
     url = models.URLField()
     emoji = models.BooleanField()
     length = models.SmallIntegerField()
+    date = models.DateTimeField(default=timezone.now)
--- serves/views.py
+++ serves/views.py
@@ -1,14 +1,21 @@
-from rest_framework import viewsets
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.viewsets import GenericViewSet
 
 from .models import Announcement, Post
 from .serializers import AnnouncementSerializer, PostSerializer
 
 
-class AnnouncementViewSet(viewsets.ModelViewSet):
+class AnnouncementViewSet(
+    GenericViewSet,
+    CreateModelMixin
+):
     serializer_class = AnnouncementSerializer
     queryset = Announcement.objects.all()
 
 
-class PostViewSet(viewsets.ModelViewSet):
+class PostViewSet(
+    GenericViewSet,
+    CreateModelMixin
+):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
